72a7a88a06811ad7edc550c8c7f56a1a7d819dc6
NGreben <96392178+NGreben@users.noreply.github.com>
2023-04-09 10:30:11 +0300

    Add files via upload

---
+++ Program_66.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int m, n;
+Console.Write("Введите М: ");
+int.TryParse(Console.ReadLine(), out m);
+Console.Write("Введите N: ");
+int.TryParse(Console.ReadLine(), out n);
+
+Console.Write($"-> {OutNumbers(m, n)}");
+
+int OutNumbers(int m, int n)
+{
+    int sum = m;
+    if (m == n)
+        return sum;
+    return sum += OutNumbers(m + 1, n);
+}
